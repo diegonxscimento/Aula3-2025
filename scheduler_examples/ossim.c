ed32b73ba2cbe3c32dffdca566b84890db04149f
Diego Nascimento <diegonxscimento@gmail.com>
2025-10-04 17:12:44 +0100

    Trabalho Atualizado

--- scheduler_examples/ossim.c
+++ scheduler_examples/ossim.c
@@ -13,6 +13,9 @@
 #include <sys/errno.h>
 
 #include "fifo.h"
+#include "sjf.h"
+#include "rr.h"
+#include "mlfq.h"
 
 #include "msg.h"
 #include "queue.h"
@@ -233,11 +236,10 @@ void check_blocked_queue(queue_t * blocked_queue, queue_t * command_queue, uint3
 
 static const char *SCHEDULER_NAMES[] = {
     "FIFO",
-/*
     "SJF",
     "RR",
     "MLFQ",
-*/
+
     NULL
 };
 
@@ -309,6 +311,15 @@ int main(int argc, char *argv[]) {
         switch (scheduler_type) {
             case SCHED_FIFO:
                 fifo_scheduler(current_time_ms, &ready_queue, &CPU);
+                break;
+            case SCHED_SJF:
+                sjf_scheduler(current_time_ms, &ready_queue, &CPU);
+                break;
+            case SCHED_RR:
+                rr_scheduler(current_time_ms, &ready_queue, &CPU);
+                break;
+            case SCHED_MLFQ:
+                mlfq_scheduler(current_time_ms, &ready_queue, &CPU);
                 break;
 
             default:
